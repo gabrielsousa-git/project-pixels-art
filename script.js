286ff1071a6317e421c3f595d172dff556ab8d18
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-05-25 16:56:05 -0300

    Requisitos 2 e 3 concluidos

---
+++ script.js
@@ -0,0 +1,27 @@
+// function createPalette() {
+//     const palette = document.createElement("div");
+//     palette.setAttribute("id", "color-palette");
+//     body.appendChild(palette);
+// }
+
+// createPalette()
+
+// function creatCollors() {
+//     let color1 = document.createElement("div");
+//     let color2 = document.createElement("div");
+//     let color3 = document.createElement("div");
+//     let color4 = document.createElement("div");
+
+//     color1.setAttribute("class", "color");
+//     color2.setAttribute("class", "color");
+//     color3.setAttribute("class", "color");
+//     color4.setAttribute("class", "color");
+
+//     palette.appendChild(color1);
+//     palette.appendChild(color2);
+//     palette.appendChild(color3);
+//     palette.appendChild(color4);
+
+// }
+
+// creatCollors()
