8284ad2c541ab3ee2b878542f561f1167de4b7aa
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-05-28 06:09:18 -0300

    Requisito 7 finalmente finalizado VQVgit add index.html style.css script.js !

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
-// function createPalette() {
-//     const palette = document.createElement("div");
-//     palette.setAttribute("id", "color-palette");
-//     body.appendChild(palette);
-// }
+const quadroDePixels = document.querySelector('#pixel-board');
+const div = document.querySelectorAll('.pixel');
 
-// createPalette()
 
-// function creatCollors() {
-//     let color1 = document.createElement("div");
-//     let color2 = document.createElement("div");
-//     let color3 = document.createElement("div");
-//     let color4 = document.createElement("div");
+function pixelBoard(size) {  
+    for (let index = 0; index < size * size; index += 1) {
+        const div = document.createElement('div')
+        div.classList.add('pixel')             
+        quadroDePixels.appendChild(div)
+    }
+}
+pixelBoard(5);
 
-//     color1.setAttribute("class", "color");
-//     color2.setAttribute("class", "color");
-//     color3.setAttribute("class", "color");
-//     color4.setAttribute("class", "color");
+let color1 = document.getElementById('color1');
+let color2 = document.getElementById('color2');
+let color3 = document.getElementById('color3');
+let color4 = document.getElementById('color4');
 
-//     palette.appendChild(color1);
-//     palette.appendChild(color2);
-//     palette.appendChild(color3);
-//     palette.appendChild(color4);
 
-// }
+const selected = document.getElementsByClassName('selected');
 
-// creatCollors()
+color1.addEventListener('click', function(){
+    if (document.getElementById("color1").classList.contains('color')) {
+        document.getElementById("color1").classList.add('selected') 
+
+        document.getElementById('color2').classList.remove('selected');
+        document.getElementById('color3').classList.remove('selected');
+        document.getElementById('color4').classList.remove('selected');
+    }    
+});
+
+color2.addEventListener('click', function(){
+    if (document.getElementById("color2").classList.contains('color')) {
+        document.getElementById("color2").classList.add('selected') 
+
+        document.getElementById('color1').classList.remove('selected');
+        document.getElementById('color3').classList.remove('selected');
+        document.getElementById('color4').classList.remove('selected');
+    }
+    
+});
+
+color3.addEventListener('click', function(){
+    if (document.getElementById("color3").classList.contains('color')) {
+        document.getElementById("color3").classList.add('selected')
+
+        document.getElementById('color1').classList.remove('selected');
+        document.getElementById('color2').classList.remove('selected');
+        document.getElementById('color4').classList.remove('selected');
+    }
+});
+
+color4.addEventListener('click', function(){
+    if (document.getElementById("color4").classList.contains('color')) {
+        document.getElementById("color4").classList.add('selected') 
+
+        document.getElementById('color1').classList.remove('selected');
+        document.getElementById('color2').classList.remove('selected');
+        document.getElementById('color3').classList.remove('selected');
+    }
+});
