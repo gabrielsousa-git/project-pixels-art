4724c0b1736b0c7ffb82b6e01e405f0f4bf4a155
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-10-31 18:58:42 -0300

    Melhorando a estilização e corrigindo erros de lint

--- script.js
+++ script.js
@@ -1,76 +1,69 @@
 const quadroDePixels = document.querySelector('#pixel-board');
-// const div = document.querySelectorAll('.pixel');
-// const color = document.querySelector('.color');
-const selected = document.getElementsByClassName('selected')[0];
 const limpar = document.querySelector('#clear-board');
 
-pixelBoard(5);
+const color1 = document.getElementById('black');
+const color2 = document.getElementById('cian');
+const color3 = document.getElementById('pourple');
+const color4 = document.getElementById('red');
 
-let color1 = document.getElementById('black');
-let color2 = document.getElementById('cian');
-let color3 = document.getElementById('pourple');
-let color4 = document.getElementById('red');
+color1.addEventListener('click', () => {
+  if (document.getElementById('black').classList.contains('color')) {
+    document.getElementById('black').classList.add('selected');
 
-// const selected = document.getElementsByClassName('selected');
-
-color1.addEventListener('click', function(){
-    if (document.getElementById("black").classList.contains('color')) {
-        document.getElementById("black").classList.add('selected') 
-
-        document.getElementById('cian').classList.remove('selected');
-        document.getElementById('pourple').classList.remove('selected');
-        document.getElementById('red').classList.remove('selected');
-    }    
+    document.getElementById('cian').classList.remove('selected');
+    document.getElementById('pourple').classList.remove('selected');
+    document.getElementById('red').classList.remove('selected');
+  }
 });
 
-color2.addEventListener('click', function(){
-    if (document.getElementById("cian").classList.contains('color')) {
-        document.getElementById("cian").classList.add('selected') 
+color2.addEventListener('click', () => {
+  if (document.getElementById('cian').classList.contains('color')) {
+    document.getElementById('cian').classList.add('selected');
 
-        document.getElementById('black').classList.remove('selected');
-        document.getElementById('pourple').classList.remove('selected');
-        document.getElementById('red').classList.remove('selected');
-    }
+    document.getElementById('black').classList.remove('selected');
+    document.getElementById('pourple').classList.remove('selected');
+    document.getElementById('red').classList.remove('selected');
+  }
 });
 
-color3.addEventListener('click', function(){
-    if (document.getElementById("pourple").classList.contains('color')) {
-        document.getElementById("pourple").classList.add('selected')
+color3.addEventListener('click', () => {
+  if (document.getElementById('pourple').classList.contains('color')) {
+    document.getElementById('pourple').classList.add('selected');
 
-        document.getElementById('black').classList.remove('selected');
-        document.getElementById('cian').classList.remove('selected');
-        document.getElementById('red').classList.remove('selected');
-    }
+    document.getElementById('black').classList.remove('selected');
+    document.getElementById('cian').classList.remove('selected');
+    document.getElementById('red').classList.remove('selected');
+  }
 });
 
-color4.addEventListener('click', function(){
-    if (document.getElementById("red").classList.contains('color')) {
-        document.getElementById("red").classList.add('selected') 
+color4.addEventListener('click', () => {
+  if (document.getElementById('red').classList.contains('color')) {
+    document.getElementById('red').classList.add('selected');
 
-        document.getElementById('black').classList.remove('selected');
-        document.getElementById('cian').classList.remove('selected');
-        document.getElementById('pourple').classList.remove('selected');
-    }
+    document.getElementById('black').classList.remove('selected');
+    document.getElementById('cian').classList.remove('selected');
+    document.getElementById('pourple').classList.remove('selected');
+  }
 });
 
-function pixelBoard(size) {  
-    for (let index = 0; index < size * size; index += 1) {
-        const div = document.createElement('div')
-        div.classList.add('pixel')             
-        
-        
-        div.addEventListener('click', function(){
-            let cor = document.querySelector('.selected')
-            let colorir = window.getComputedStyle(cor)
-            cor.backgroundColor = colorir.getPropertyValue('background-color')
-            if (div.classList.contains('pixel')) 
-            div.style.backgroundColor = cor.backgroundColor
-        })
-        quadroDePixels.appendChild(div)
-    }
+function pixelBoard(size) {
+  for (let index = 0; index < size * size; index += 1) {
+    const div = document.createElement('div');
+    div.classList.add('pixel');
+
+    div.addEventListener('click', () => {
+      const cor = document.querySelector('.selected');
+      const colorir = window.getComputedStyle(cor);
+      cor.backgroundColor = colorir.getPropertyValue('background-color');
+      if (div.classList.contains('pixel')) div.style.backgroundColor = cor.backgroundColor;
+    });
+    quadroDePixels.appendChild(div);
+  }
 }
 
-limpar.addEventListener('click', function(){
-    quadroDePixels.innerHTML = ''
-    pixelBoard(5);
-})
+pixelBoard(16);
+
+limpar.addEventListener('click', () => {
+  quadroDePixels.innerHTML = '';
+  pixelBoard(16);
+});
